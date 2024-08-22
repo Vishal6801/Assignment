c08f4e318cfce1d8796f73627d23c55682851a62
Vishal A <103107847+Vishal6801@users.noreply.github.com>
2024-08-22 12:54:02 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,28 @@
+package day4;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		String mobileName ="Redmi";
+		String modelNumber ="12 5G";
+		int mobilePrice = 13999;
+		float discount = 12.5f;
+		double SARValue = 9.87473624d;
+		char logoFirst = 'M';
+		char logoSecond = 'k';
+		long IMEINumber = 86319906325264l;
+		short cashBack = 1;
+		boolean isExpensive = true;
+		System.out.println(mobileName);
+		System.out.println(modelNumber);
+		System.out.println(mobilePrice);
+		System.out.println(discount);
+		System.out.println(SARValue);
+		System.out.println(logoFirst+""+logoSecond);
+		System.out.println(IMEINumber);
+		System.out.println(cashBack);
+		System.out.println(isExpensive);
+	}
+
+}
