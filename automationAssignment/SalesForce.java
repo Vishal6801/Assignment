b3cbf651bb23312aa9e96c6ef0a61ec10cec4b88
Vishal A <103107847+Vishal6801@users.noreply.github.com>
2024-09-10 20:26:48 +0530

    Add files via upload

---
+++ automationAssignment/SalesForce.java
@@ -0,0 +1,33 @@
+package automationAssignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SalesForce {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("https://login.salesforce.com");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("vinothkumar@testleaf.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SeleniumChallenge@24");
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+		driver.findElement(By.xpath("//div[@class ='slds-r2']")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'View All')]")).click();
+		driver.findElement(By.xpath("//p[text() ='Sales']")).click();
+		WebElement clickLead = driver.findElement(By.xpath("//span[text() ='Leads']"));
+		driver.executeScript("arguments[0].click()", clickLead);
+		driver.findElement(By.xpath("//button[@name ='New']")).click();
+		driver.findElement(By.xpath("//input[@name = 'firstName']")).sendKeys("Vishal");
+		driver.findElement(By.xpath("//input[@name = 'lastName']")).sendKeys("A");
+		driver.findElement(By.xpath("//input[@name = 'Company']")).sendKeys("Testleaf");
+		driver.findElement(By.xpath("(//button[text() = 'Save'])[2]")).click();
+
+	}
+
+}
